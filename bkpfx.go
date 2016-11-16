1dad80c417d2facd3201b7c8df3885957182e283
Rudi Sluijtman <rws@sluijtman.space>
2016-11-16 11:29:25 +0100

    - cmd variable for reuse - Wait on output - := only to declare new variables

--- bkpfx.go
+++ bkpfx.go
@@ -20,15 +20,21 @@ func main(){
   }
   // copy files: /etc/postfix directory + possibly /etc/aliases
   // start postconf and read output
-  out,e := exec.Command( "/usr/sbin/postconf", "-n" ).Output()
+  cmd := exec.Command( "/usr/sbin/postconf", "-n" )
+  out,e := cmd.Output()
   if ( e != nil ) {
     log.Fatal(e)
   }
+
+  if e = cmd.Wait() ; e != nil {
+    fmt.Println( e )
+  }
+
   fmt.Print(string(out))
   
 
   fmt.Println( "-------Alernative-----" )
-  cmd := exec.Command( "/usr/sbin/postconf", "-n" )
+  cmd = exec.Command( "/usr/sbin/postconf", "-n" )
   stdout, e := cmd.StdoutPipe()
   cmd.Start()
   r := bufio.NewReader(stdout)
@@ -36,7 +42,7 @@ func main(){
   for ; e == nil ; line, _, e = r.ReadLine() {
     fmt.Println( string(line) )
   }
-  if e := cmd.Wait(); e != nil {
+  if e = cmd.Wait() ; e != nil {
     fmt.Println( e )
   }
   fmt.Println( "-----------------------" )
