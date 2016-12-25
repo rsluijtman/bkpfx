4c968db42c0c51355c44c377b2229ebf4de4bbe3
Rudi Sluijtman <rws@sluijtman.space>
2016-12-25 17:18:04 +0100

    replace variables with value

--- bkpfx.go
+++ bkpfx.go
@@ -6,8 +6,41 @@ import (
   "io/ioutil"
   "os/exec"
   "regexp"
+  "sort"
   "syscall"
 )
+
+
+// var rxvar = regexp.MustCompile( `\$[0-9a-zA-Z_]+` ) 
+var rxvar = regexp.MustCompile( `\$\w+` ) 
+var inited bool = false
+
+func substitutevars( val *string, cfgm *map[string]string ) {
+  if ( inited == false ) { 
+    inited = true
+  }
+  var loc []int 
+  loc = rxvar.FindIndex( []byte( *val ) ) 
+  if ( loc != nil ) {
+    var before []byte
+    var after []byte
+    if ( loc[0] > 0 ) {
+      before = val[ : loc[0]-1 ]
+    }
+    if ( loc[1] < val.len ) {
+      after=val[ loc[1]+1 : ]
+    }
+    var tmp string
+    var newval string
+
+    tmp = "found var: "+ *val + "-> "
+    newval = string( before ) +
+             cfgm[ string( val[ loc[0]+1:loc[1] ] ) ] +
+             string( after )
+    *val = tmp + newval
+  }
+  // return val 
+}
 
 var cfgdir = "/etc/postfix"
 func main(){
@@ -20,6 +53,7 @@ func main(){
   // copy files: /etc/postfix directory + possibly /etc/aliases
 
   // start postconf and read output
+  var configkey []string
   var configmap map[string]string
   configmap = make( map[string]string )
   cmd := exec.Command( "/usr/sbin/postconf" )
@@ -38,12 +72,19 @@ func main(){
       fmt.Printf( "key: %s - value: %s\n", string( match[1] ),
                    string( match[2]) )
       configmap[ string( match[1] ) ]  = string( match[2] )
+      configkey = append( configkey, string(match[1]) ) 
     }
   }
-
+  fmt.Println( "------------------" ) 
   for key, value := range configmap {
     fmt.Println("Key:", key, "Value:", value)
   }
+  fmt.Println( "------------------" ) 
+  sort.Strings( configkey )
+  for _, key := range( configkey ) {
+    fmt.Println( "key:", key, "value:", configmap[key] )
+  }
+  fmt.Println( "------------------" ) 
 
   /* files do not need to be in config_directory, but usually this
      is only aliases, so only lookup alias_database, then copy files from
@@ -57,5 +98,12 @@ func main(){
   }
   for i, file := range files {
     fmt.Printf( "%4d %s\n", i, file.Name() )
+  }
+
+  fmt.Println( "------------------" ) 
+  // susbstitute $config_directory
+  for key, value := range configmap {
+    substitutevars( &value )
+    fmt.Println("Key:", key, "Value:", value)
   }
 }
