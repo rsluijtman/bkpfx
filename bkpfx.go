ee2cce3bc57de11b44f8779fe8706872e8553ade
Rudi Sluijtman <rws@sluijtman.space>
2017-01-03 14:05:20 +0100

    keep substituting all untill nothing is changed this is in a for loop over all map elements, should be done over single elements

--- bkpfx.go
+++ bkpfx.go
@@ -13,24 +13,28 @@ import (
 
 var rxvar = regexp.MustCompile( `\$[0-9a-zA-Z_]+` ) 
 // var rxvar = regexp.MustCompile( `\$\w+` ) 
-var notfirst bool;
+var notfirst bool
+var debug int
 
 func substitutevars( val *string, cfgm map[string]string ) {
   var loc []int 
   var bval []byte ;
 
-  if notfirst==false {
-    notfirst=true
-    for k,v := range( cfgm ) {
-      println( k,v, cfgm[k] )
+  if debug >=3 {
+    if notfirst==false {
+      notfirst=true
+      for k,v := range( cfgm ) {
+        println( k,v, cfgm[k] )
+      }
+      println( "------------------" )
     }
-    println( "------------------" )
   }
+
   bval=[]byte(*val)
   loc = rxvar.FindStringIndex( *val ) 
   if ( loc != nil ) {
-println("val :",val,*val)
-println( "match loc[0]: ", loc[0], "loc[1]: ", loc[1] )
+    println("val :",val,*val)
+    println( "match loc[0]: ", loc[0], "loc[1]: ", loc[1] )
     var before []byte
     var after []byte
     if ( loc[0] > 0 ) {
@@ -44,28 +48,20 @@ println( "match loc[0]: ", loc[0], "loc[1]: ", loc[1] )
     bkey = bval[ loc[0]+1:loc[1] ] 
     
     var key string
-    // key has/needs trailing space !!
-    // key=string(bkey ) + " "
     key=string(bkey )
-    // tmp = "found var: "+ *val + "-> "
     if v,e := cfgm[key];e {
       println( "replacing key: ",key,"with value:",v )
     } else {
       println( "cfgm",key,"is not defined",e )
       return
     }
-
     newval = string( before ) + cfgm[ key ] + string( after )
-    // newval = string( before ) + v + string( after )
-    // *val = tmp + newval
     *val = newval
   }
-  // return val 
 }
 
 var cfgdir = "/etc/postfix"
 func main(){
-  var debug int
   e := syscall.Chdir( cfgdir )
   if  e != nil  {
     fmt.Printf( "chdir %s failed: %s\n", cfgdir, e.Error() )
@@ -94,8 +90,10 @@ func main(){
     if match == nil {
       fmt.Printf( "no match: %s\n", string(line) )
     } else {
-/*      fmt.Printf( "key: %s - value: %s\n", string( match[1] ),
-                   string( match[2]) ) */
+      if debug > 11 {
+        fmt.Printf( "key: %s - value: %s\n", string( match[1] ),
+                     string( match[2]) )
+      }
       configmap[ string( match[1] ) ]  = string( match[2] )
       configkey = append( configkey, string(match[1]) ) 
     }
@@ -130,12 +128,40 @@ func main(){
   }
 
   fmt.Println( "------------------" ) 
-  // susbstitute $config_directory
+  var configvaluemap map[string]string
+  configvaluemap = make( map[string]string )
   var key string
   var value string
   for key, value = range configmap {
     substitutevars( &value, configmap )
+    configvaluemap[key]=value
     fmt.Println("Key:", key, "Value:", value)
-    println( key,configmap[key])
+    println( "-",key,"-",configmap[key],"-",configvaluemap[key])
   }
+  var changed bool
+  for changed=true ; changed ; {
+    fmt.Println( "------------------" ) 
+    changed=false
+    for key,value = range configvaluemap {
+      substitutevars( &value, configvaluemap )
+      if value != configvaluemap[key] {
+        changed=true
+        configvaluemap[key]=value
+        fmt.Println("Key:", key, "Value:", value)
+        println( "+",key,"+",configmap[key],"+",configvaluemap[key])
+      }
+    }
+  }
+  println( "no changes detected" )
+  fmt.Println( "------------------" ) 
+
+  debug=11
+  if debug > 10 {
+    sort.Strings( configkey )
+    for _, key := range( configkey ) {
+      fmt.Println( "key:", key, "value:", configvaluemap[key] )
+    }
+    fmt.Println( "------------------" ) 
+  }
+
 }
